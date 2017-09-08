84b74894b72cbe9e5c6d3f9945aefdeb82efe048
Eric Lee <ericlee@sympatico.ca>
2017-09-07 20:53:37 -0400

    Sale price of qtyEnforcedGroupPM The change is already upstream. cron/tasks/SalesBatchTask.php

--- fannie/cron/tasks/SalesBatchTask.php
+++ fannie/cron/tasks/SalesBatchTask.php
@@ -169,6 +169,11 @@ class SalesBatchTask extends FannieTask
                 // list of UPCs that should be on sale
                 $sale_upcs[] = $upc;
 
+                // for qtyEnforcedGroupPM the salePrice is the whole group price
+                if ($specialpricemethod == 2) {
+                    $special_price = $product->normal_price();
+                }
+
                 $changed = false;
                 if ($product->special_price() != $special_price) {
                     $changed = true;
